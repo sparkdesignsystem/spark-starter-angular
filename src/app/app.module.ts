7c4be7329573f20a04d2697b101f94973e54b680
Bob Wise <bobwise@quickenloans.com>
2019-12-18 13:54:18 -0500

    without spark commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,9 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
-import { SparkAngularModule } from "@sparkdesignsystem/spark-angular";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    SparkAngularModule,
-    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule
   ],
