bf58d6051ed3e377e3558157e0c18e7c1e44871a
Bob Wise <bobwise@quickenloans.com>
2020-04-20 17:01:19 -0400

    new components

--- src/app/spark-docs/centered-column-docs/centered-column-docs.component.ts
+++ src/app/spark-docs/centered-column-docs/centered-column-docs.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'sprk-centered-column-docs',
   template: `
+  <div sprkCenteredColumn class='sprk-u-BackgroundColor--green'>
+    This is a centered column with a green background
+  </div>
   `
 })
 export class CenteredColumnDocsComponent {
