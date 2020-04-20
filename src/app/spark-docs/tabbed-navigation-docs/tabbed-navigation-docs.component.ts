2132ad516676fb88793f6a016a46e078bdd2bc73
Bob Wise <bobwise@quickenloans.com>
2020-04-20 15:16:52 -0400

    more useful tab content

--- src/app/spark-docs/tabbed-navigation-docs/tabbed-navigation-docs.component.ts
+++ src/app/spark-docs/tabbed-navigation-docs/tabbed-navigation-docs.component.ts
@@ -13,19 +13,13 @@ import { Component } from '@angular/core';
         <button [defaultActive]=true sprkTabbedNavigationTab>Tab 2</button>
         <button sprkTabbedNavigationTab>Tab 3</button>
         <div sprkTabbedNavigationPanel>
-          <p>Tab 1 Content Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis rhoncus ipsum. Nulla
-            euismod nisi est, vel consequat ante consectetur in. Ut interdum dictum est at ornare. Nam nec dapibus nibh.
-            Integer venenatis ex eu mi euismod, non ultricies lacus venenatis.</p>
+          <p>Tab One Tab One Tab One Tab One Tab One Tab One Tab One Tab One Tab One Tab One Tab One</p>
         </div>
         <div [defaultActive]="true" sprkTabbedNavigationPanel>
-          <p>Tab 2 Content Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis rhoncus ipsum. Nulla
-            euismod nisi est, vel consequat ante consectetur in. Ut interdum dictum est at ornare. Nam nec dapibus nibh.
-            Integer venenatis ex eu mi euismod, non ultricies lacus venenatis.</p>
+          <p>Tab Two Tab Two Tab Two Tab Two Tab Two Tab Two Tab Two Tab Two Tab Two Tab Two Tab Two</p>
         </div>
         <div sprkTabbedNavigationPanel>
-          <p>Tab 3 Content Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis rhoncus ipsum. Nulla
-            euismod nisi est, vel consequat ante consectetur in. Ut interdum dictum est at ornare. Nam nec dapibus nibh.
-            Integer venenatis ex eu mi euismod, non ultricies lacus venenatis.</p>
+          <p>Tab Three Tab Three Tab Three Tab Three Tab Three Tab Three Tab Three Tab Three Tab Three Tab Three Tab Three</p>
         </div>
       </sprk-tabbed-navigation>
     </div>
