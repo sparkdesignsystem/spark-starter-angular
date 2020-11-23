b2dedcc640cc68a739d83c9cd72b6e6afe41bb25
corcorankt1 <guern1kn@gmail.com>
2020-11-23 16:35:44 -0500

    add additional inputs to stack and accordion

--- src/app/spark-docs/accordion-docs/accordion-docs.component.ts
+++ src/app/spark-docs/accordion-docs/accordion-docs.component.ts
@@ -5,14 +5,16 @@ import { Component } from '@angular/core';
   template: `
     <div class="sprk-demo-o-ContentGrouping">
       <h2 class="sprk-demo-b-h2">Primary Accordion</h2>
-      <sprk-accordion>
+      <sprk-accordion idString="accordion-1" additionalClasses="accordion-test-class">
         <sprk-accordion-item
+          additionalClasses="accordion-item-test-class"
           idString="accordion-item-1"
           analyticsString="object.action.event"
           headingAdditionalClasses="sprk-b-TypeDisplaySeven"
           contentAdditionalClasses="sprk-b-BodyTypeTwo"
           isOpen="true"
           heading="This is a longer title than all the other accordion titles here. But, it works!"
+          iconAdditionalClasses="icon-test-class"
         >
           <p>
             This is some content This is some content. This is some content.
@@ -24,6 +26,8 @@ import { Component } from '@angular/core';
           headingAdditionalClasses="sprk-b-TypeDisplaySeven"
           contentAdditionalClasses="sprk-b-BodyTypeTwo"
           heading="This is another title"
+          iconNameClosed="arrow-right"
+          iconNameOpen="arrow-left"
         >
           <p>This is some more content.</p>
         </sprk-accordion-item>
@@ -31,6 +35,8 @@ import { Component } from '@angular/core';
           headingAdditionalClasses="sprk-b-TypeDisplaySeven"
           contentAdditionalClasses="sprk-b-BodyTypeTwo"
           heading="This is another title"
+          leadingIcon="exclamation"
+          leadingIconAdditionalClasses="leading-icon-test-class"
         >
           <p>This is some more content.</p>
         </sprk-accordion-item>
